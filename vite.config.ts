168596566a9bd3d0f4563e46190f128ff783c1c2
Ryan Goree <goree.ryan@gmail.com>
2024-03-04 19:03:16 -0600

    Addd tsconfig-paths, configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  let basePath = process.env.BASE_PATH;
+  if (!basePath) {
+    const env = loadEnv(mode, process.cwd(), "");
+    basePath = env.BASE_PATH;
+  }
+  return {
+    plugins: [react(), tsconfigPaths()],
+    envDir: ".",
+    base: basePath ?? "/",
+  };
+});
