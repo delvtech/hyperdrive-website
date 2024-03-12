211fc04356a5294e765cae8bf51ad7958b06942a
Ryan Goree <goree.ryan@gmail.com>
2024-03-12 06:27:46 -0500

    Add scroll reset on nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Build } from "src/pages/Build";
 import { Home } from "src/pages/Home";
+import { ScrollReset } from "./components/router/ScrollReset";
 
 function App() {
   return (
     <>
+      <ScrollReset />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/build" element={<Build />} />
